290463805dbe5d22143969f7be0b1f30b2cc3e22
Hemanth Kumar P <hemanth.pitta21821@outlook.com>
2025-06-29 00:42:58 +0530

    Create For_Each.tf

---
+++ For_Each.tf
@@ -0,0 +1,39 @@
+ # We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.21.1"
+    }
+  }
+}
+
+#Variable defining
+variable "client_id" {}
+variable "tenant_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "location" {}
+variable "resource_groups" {
+  default = {
+    dev = "East US"
+    prod = "West Europe"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  client_id = var.client_id
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  client_secret = var.client_secret
+}
+
+ # Create Azure Resource group
+resource "azurerm_resource_group" "commerce_rg" {
+  for_each = var.resource_groups
+  name = "${each.key}-rg"
+  location = each.value
+}
